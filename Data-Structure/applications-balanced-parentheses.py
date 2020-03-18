a24156453b3f807fc37b2be9a3a5b59d90872fe7
Zijing Zhu <55524390+zijing0926@users.noreply.github.com>
2020-03-18 14:55:20 -0700

    Create applications-balanced-parentheses.py

---
+++ Data-Structure/applications-balanced-parentheses.py
@@ -0,0 +1,27 @@
+##check balanced parentheses
+def parChecker(strings):
+    s=Stack()
+		balanced=True
+		i=0
+		while i <=len(strings)) and balanced:
+		    symbol=strings[i]
+				if symbol="([{":
+				    s.push(symbol)
+				else:
+				    if s.isEmpty():
+						    balanced=False
+						else:
+				        top=s.pop()
+								if not matches(top,symbol):
+								       balanced= False
+			   i+=1
+		 if balanced and s.isEmpty():
+		    return True
+		 else:
+		    return False
+				
+def matches(open,close):
+    opens="([{"
+		closes=")]}"
+		return opens.index(open)==closes.index(close)
+		   
